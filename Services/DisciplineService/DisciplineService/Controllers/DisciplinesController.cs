1ac85fd32d17e1165365471b0a1d8416100f1e00
Koval Vadym <vzlobinkov@gmail.com>
2022-10-07 22:12:22 +0300

    Add Discipline Service

---
+++ Services/DisciplineService/DisciplineService/Controllers/DisciplinesController.cs
@@ -0,0 +1,59 @@
+﻿using DisciplineService.Interfaces;
+using DisciplineService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DisciplineService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DisciplinesController : ControllerBase
+    {
+        private readonly IDisciplineService _disciplineService;
+
+        public DisciplinesController(IDisciplineService disciplineService)
+        {
+            _disciplineService = disciplineService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DisciplineModel>>> Get()
+        {
+            var disciplines = await _disciplineService.GetAllAsync();
+
+            return Ok(disciplines);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DisciplineModel>> Get(int id)
+        {
+            var discipline = await _disciplineService.GetByIdAsync(id);
+
+            return Ok(discipline);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _disciplineService.DeleteByIdAsync(id);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, DisciplineModel model)
+        {
+            await _disciplineService.UpdateAsync(id, model);
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(DisciplineModel model)
+        {
+            var created = await _disciplineService.AddAsync(model);
+
+            return CreatedAtAction(nameof(Add), new { id = created.Id }, created);
+        }
+    }
+}
---
+++ Services/DisciplineService/DisciplineService/Entities/Discipline.cs
@@ -0,0 +1,14 @@
+﻿using DisciplineService.Enums;
+
+namespace DisciplineService.Entities
+{
+    public class Discipline : BaseEntity
+    {
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public int Course { get; set; }
+        public CreditType CreditType { get; set; }
+        public int Hours { get; set; }
+        public bool IsSelective { get; set; }
+    }
+}
