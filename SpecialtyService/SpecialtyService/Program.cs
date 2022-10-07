4653b2be045cb59e424252d8460abede78178511
bbodya <71715505+bbodya@users.noreply.github.com>
2022-10-07 10:47:31 +0300

    add Specialty Service

---
+++ SpecialtyService/SpecialtyService/Program.cs
@@ -0,0 +1,40 @@
+using AutoMapper;
+using Business.Service;
+using Business.Interfaces;
+using Data_access.Interfaces;
+using Data_access.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ISpecialtyRepository, SpecialtyRepository>();
+builder.Services.AddSingleton<ISpecialtyService, SpecialtyService>();
+
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new AutomapperProfile());
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
