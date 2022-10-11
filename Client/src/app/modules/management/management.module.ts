230d5c3a4f03ca742f526dfe5f76524a7382d41c
Koval Vadym <vzlobinkov@gmail.com>
2022-10-11 17:24:34 +0300

    Add header

--- Client/src/app/modules/management/management.module.ts
+++ Client/src/app/modules/management/management.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { DisciplinesComponent } from './components/disciplines/disciplines.component';
 import { ManagementRoutingModule } from './management-routing.module';
 import { FacultyComponent } from './components/faculty/faculty.component';
+import { ManagementPageComponent } from './management-page/management-page.component';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @NgModule({
   declarations: [
     DisciplinesComponent,
-    FacultyComponent
+    FacultyComponent,
+    ManagementPageComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
