b192866712d5b1d07f53fec6d43ca32f742f634c
bbodya <71715505+bbodya@users.noreply.github.com>
2022-10-16 02:39:06 +0300

    faculty fronend

--- Client/src/app/modules/management/components/faculty/faculty.component.ts
+++ Client/src/app/modules/management/components/faculty/faculty.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Faculty } from '../../models/faculty.model';
-import { FacultyService } from '../../services/faculty.service';
+//import { FacultyService } from '../../services/faculty.service';
 
 @Component({
   selector: 'app-faculty',
@@ -10,13 +10,14 @@ import { FacultyService } from '../../services/faculty.service';
 export class FacultyComponent implements OnInit {
   faculties: Faculty[]=[];
 
-  constructor(private facultyService: FacultyService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.facultyService.get()
-    .subscribe(result => {
-      this.faculties=result
-    });
+    this.faculties = [
+      { Id: 1, Name: 'Faculty 1', Description: "Faculty 1 Dription. Faculty 1 Description. Faulty 1 dssd Desription", Disciplines: 103, Teachers: 23, Specialties: 3},
+      { Id: 2, Name: 'Faculty 2', Description: "Faculty 2 Dription. Faculty 2 Description. Faulty 2 dssd Desription", Disciplines: 83, Teachers: 17, Specialties: 1},
+      { Id: 3, Name: 'Faculty 3', Description: "Faculty 3 Dription. Faculty 3 Description. Faulty 3 dssd Desription", Disciplines: 92, Teachers: 41, Specialties: 7}
+    ];
   }
 
 }
