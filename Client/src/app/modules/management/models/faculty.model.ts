6a286d90aba8f12d1cf7444b5e8a0ae35940ca08
bbodya <71715505+bbodya@users.noreply.github.com>
2022-10-16 02:44:49 +0300

    frontend

--- Client/src/app/modules/management/models/faculty.model.ts
+++ Client/src/app/modules/management/models/faculty.model.ts
@@ -2,4 +2,7 @@ export class Faculty {
     public Id: number;
     public Name: string;
     public Description: string;
+    public Disciplines: number;
+    public Teachers: number;
+    public Specialties: number;
 }
