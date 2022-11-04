e08392d75fc4c758a498a56e1ebf7f38b7213a86
Koval Vadym <vzlobinkov@gmail.com>
2022-11-04 19:24:16 +0200

    Add faculties page/crud

--- Client/src/app/modules/management/services/faculty.service.ts
+++ Client/src/app/modules/management/services/faculty.service.ts
@@ -2,15 +2,29 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Faculty } from '../models/faculty.model';
 import { Observable } from 'rxjs/internal/Observable';
-import { environment } from 'src/environments/environment';
+import { environment, HEADERS } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FacultyService {
   constructor(private _http: HttpClient) { }
 
     get(): Observable<Faculty[]>{
       return this._http.get<Faculty[]>(environment.urlPrefix + environment.facultyUrl);
     }
+
+    getById(id: number): Observable<Faculty> {
+      return this._http.get<Faculty>(environment.urlPrefix + environment.facultyUrl + `/${id}`);
+    }
+
+    create(faculty: Faculty) {
+      console.log('create method of Faculty Service');
+      return this._http.post<Faculty>(environment.urlPrefix + environment.facultyUrl, faculty);
+
+      /*const body = JSON.stringify(faculty);
+
+      return this._http.post<Faculty>(environment.urlPrefix + environment.facultyUrl, body, {"headers": HEADERS});*/
+  }
 }
