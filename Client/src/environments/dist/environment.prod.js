9ba2957ec5bc5f6981cee1eec46fefa15115549d
Hurmaze <torgmas13@gmail.com>
2022-11-29 23:46:51 +0200

    looks like cool

---
+++ Client/src/environments/dist/environment.prod.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    facultyUrl: 'api/faculties',
+    teachersUrl: 'api/teachers',
+    specialtiesUrl: 'api/specialties',
+    disciplinesUrl: 'api/disciplines',
+    catalogsUrl: 'api/disciplines/catalogs',
+    groupsUrl: 'api/groups',
+    catalogDisciplinesUrl: 'api/disciplines/catalogDisciplines',
+    disciplineTeachersUrl: 'api/disciplines/disciplineTeachers',
+    specialtyDisciplinesUrl: 'api/disciplines/specialtyDisciplines',
+    facultySpecialtyUrl: 'api/specialties/facultySpecialties',
+    facultyDisciplineUrl: 'api/disciplines/facultyDisciplines',
+    facultyTeacherUrl: 'api/teachers/facultyTeachers',
+    whiteListedDomains: [''],
+    usersUrl: 'api/tokens',
+    urlPrefix: 'http://192.168.59.120/'
+};
